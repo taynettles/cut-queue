af3158ce4a76d15f3623671b3ec731742bdc5b75
Taylen Nettles <taynettles@gmail.com>
2025-12-10 22:00:24 -0600

    core layout and provider component

---
+++ components/providers.tsx
@@ -0,0 +1,9 @@
+// Wrap app with necessary providers
+
+"use client"
+
+import { SessionProvider } from "next-auth/react"
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    return <SessionProvider>{children}</SessionProvider>
+}
